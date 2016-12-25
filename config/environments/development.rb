a4eaed5391c0f77e9c0d80c4fe0e914e1b8feb3a
Tasha25 <greennatasha@aol.com>
2016-12-25 15:03:04 -0500

    Clean up merge

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,6 +51,9 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+<<<<<<< HEAD
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+=======
+>>>>>>> parent of 8ea01cd... Add and setup Devise
 end
